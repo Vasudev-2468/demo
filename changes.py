8e3a0cb1ded1ae990fe97fe50ed64582c9ceb6fa
Vasudev-2468 <svasudevanidvrs@gmail.com>
2024-03-18 07:36:50 +0530

    feat: important line

---
+++ changes.py
@@ -0,0 +1,13 @@
+# Python Vote code
+num = int(input("Enter the number: "))
+if age > num:
+  print("eligible to vote")
+else:
+  print("not eligible to vote")
+  print("This is an important line")
+
+
+# I am very good to have this code
+  
+
+print("This is my good code")
